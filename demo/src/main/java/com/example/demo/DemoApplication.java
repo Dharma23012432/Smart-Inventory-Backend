84c10f7328208fb226505f8bbc53b672545b8233
Dharma23012432 <152275002+Dharma23012432@users.noreply.github.com>
2025-12-31 09:47:42 +0530

    Update DemoApplication.java

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAsync
 @SpringBootApplication // ✅ Make sure there's NO exclude here
 public class DemoApplication {
     public static void main(String[] args) {
